2e03fec4c004f58993262da3ac6068751ef2c0ce
Carlos Andrade <dev_carlos@outlook.com>
2022-03-02 22:09:36 -0300

    vue 3

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from "@/components/welcome/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+## Front-End
+
+Front end com Vue
+
+### Instalando
+
+`npm install`
+`npm run lint`
+`npm run dev`
+
+
+## Back-End
+
+Back end com java usando microserviços
+
